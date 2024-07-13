c57d15b77d5277be39074ff9833d23442be21189
oshai-coder <eoshai07@yahoo.com>
2024-07-13 09:09:31 +0000

    oshiacoder

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "oshai"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "emma"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "oshai"
 )
 
 var ID ids.ID
